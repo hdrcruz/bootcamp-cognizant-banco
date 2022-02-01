ada1e3be51d5b724903747f6be656e527288952d
Helder Cruz <hdr.cruz@gmail.com>
2022-01-31 22:33:34 -0300

    incluir códigos de estudo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Cliente helder = new Cliente();
+		helder.setNome("Helder Cruz");
+		
+		Conta cc = new ContaCorrente(helder);
+		Conta poupanca = new ContaPoupanca(helder);
+
+		cc.depositar(3000);
+		cc.transferir(650, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
     }
 }
